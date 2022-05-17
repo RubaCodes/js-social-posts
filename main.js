bdc8022b6fc5eed1534627cb438f3ad5a6c03a04
rubacodes <rubacodes@gmail.com>
2022-05-17 09:56:25 +0200

    aggiunot id ai post, non lo avevo visto lol

--- main.js
+++ main.js
@@ -89,7 +89,7 @@ function newPost(post) {
     <div class="post__footer">
       <div class="likes js-likes">
         <div class="likes__cta">
-          <a class="like-button js-like-button" href="#" data-postid="1">
+          <a class="like-button js-like-button" href="#" data-postid="${post.id}">
             <i
               class="like-button__icon fas fa-thumbs-up"
               aria-hidden="true"
